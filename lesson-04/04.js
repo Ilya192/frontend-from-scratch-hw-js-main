51a0ce022cdb79ea6bb469bcbb8782a77da3fc9f
Ilya192 <leprekon92@yandex.ru>
2025-01-29 22:51:45 +0400

    fix Function

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -11,5 +11,9 @@
 
 Пример работы функции: `doubleEachCharacter('hello')` вернёт `'hheelllloo'`
 */
+let str = ("Hi")
+function doubleEachCharacter("Hi") {
 
-function doubleEachCharacter() {}
+    
+}
+alert(str)
